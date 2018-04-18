fa78335cae53f05ed145aa864ada46afad284fa3
Victoria Betts <toribetts@gmail.com>
2018-04-18 18:19:08 -0400

    Remove old directory

--- furbnb-web/src/actions/actions.js
+++
@@ -1,8 +0,0 @@
-import { TOGGLE_CREATE_HOST, SELECT_CITY } from '../constants/constants'
-
-export function toggleCreateHost(isHost){
-	return { type : TOGGLE_CREATE_HOST, isHost }
-}
-export function selectCity(citySelection){
-	return { type : SELECT_CITY, citySelection }
-}
