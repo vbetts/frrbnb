a01641fd39b83a368ceac858879b718499bb2726
Victoria Betts <toribetts@gmail.com>
2018-04-18 18:03:38 -0400

    Updates to actions and reducers

--- furbnb-web/src/actions/actions.js
+++ furbnb-web/src/actions/actions.js
@@ -1,5 +1,8 @@
-import { TOGGLE_CREATE_HOST } from '../constants/constants'
+import { TOGGLE_CREATE_HOST, SELECT_CITY } from '../constants/constants'
 
 export function toggleCreateHost(isHost){
 	return { type : TOGGLE_CREATE_HOST, isHost }
+}
+export function selectCity(citySelection){
+	return { type : SELECT_CITY, citySelection }
 }
--- furbnb-web/src/reducers/reducers.js
+++ furbnb-web/src/reducers/reducers.js
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux'
-import { TOGGLE_CREATE_HOST } from '../constants/constants'
+import { TOGGLE_CREATE_HOST, SELECT_CITY } from '../constants/constants'
 
 function accounts(state=[], action){
 	switch (action.type){
 		case TOGGLE_CREATE_HOST:
 			return Object.assign({}, state, {
 				isHost: !state.isHost
+			});
+		case SELECT_CITY:
+			return Object.assign({}, state, {
+				citySelection: action.citySelection
 			});
 		default:
 			return state
